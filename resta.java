bbd1a9b153d13c8e716e9c7fe422624fb513a9c6
Yarias2106 <Zekrom2110@gmail.com>
2021-10-10 15:41:01 -0400

    agregando resta.javaI:

---
+++ resta.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class resta here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class resta
+{
+    public static void main (String args[]){
+       int a=15;
+       int b=8;
+       int c=a-b;
+       System.out.println(c);
+    
+    }
+}
