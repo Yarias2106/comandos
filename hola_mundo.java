c646cbb6d64cf1921fa172ae2e071a475d4010e3
Yarias2106 <Zekrom2110@gmail.com>
2021-10-10 15:14:38 -0400

    primer commit de hola mundo

---
+++ hola_mundo.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class hola_mundo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class hola_mundo
+{
+    public static void main(String args[]){
+
+        System.out.print("Hola Mundo");
+    }
+}
