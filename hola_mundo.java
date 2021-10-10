9e11389e7c78b3aea05217cbc4c68f1de0f30557
Yarias2106 <Zekrom2110@gmail.com>
2021-10-10 15:38:30 -0400

    modificando hoala_mundo y suma

--- hola_mundo.java
+++ hola_mundo.java
@@ -10,5 +10,6 @@ public class hola_mundo
     public static void main(String args[]){
 
         System.out.print("Hola Mundo");
+          System.out.print("Hola Mundo");
     }
 }
---
+++ suma.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class suma here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class suma
+{
+   public static void main (String args[]){
+       int a=5;
+       int b=8;
+       int c=a+b;
+       System.out.println(c);
+     System.out.println(c);
+    
+    }
+}
